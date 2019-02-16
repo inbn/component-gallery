bc7b9dfc1f8ef0531a3f35e4c9c329715b5db889
Iain Bean <iainmbean@gmail.com>
2019-02-16 18:34:32 +0000

    Enable gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
     'gatsby-transformer-remark',
     {
       resolve: `gatsby-source-airtable`,
