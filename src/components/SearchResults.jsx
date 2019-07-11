c4d5a5cf46f5b9f78c6a868b3fa1fa73a11094fc
Iain Bean <iainmbean@gmail.com>
2019-07-11 20:14:53 +0100

    WIP on site search keyboard interactivity

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,37 +1,7 @@
 import React from 'react';
 
 const SearchResults = ({ query, results }) => (
-  <section aria-label="Search results for all posts">
-    {!!results.length && query && (
-      <p
-        className="search-results-count sr-only"
-        id="search-results-count"
-        aria-live="polite"
-        aria-atomic="true"
-      >
-        Found {results.length} results for "{query}"
-      </p>
-    )}
 
-    {!!results.length && (
-      <ol className="search-results-list">
-        {results.map(({ table, name, url, description, otherNames }) => (
-          <li key={name}>
-            {table && (
-              <p className="font-sans mb-2 uppercase text-grey-700 text-xs inline-block">
-                {table === 'Components' ? 'Component' : 'Design System'}
-              </p>
-            )}
-            <h3 className="mt-0">
-              <a href={url}>{name}</a>
-            </h3>
-            {otherNames && <p>Other names: {otherNames}</p>}
-            {description && <p>{description}</p>}
-          </li>
-        ))}
-      </ol>
-    )}
-  </section>
 );
 
 export default SearchResults;
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { graphql } from 'gatsby';
+import React, { useCallback, useEffect, useState } from 'react';
+import { navigate, graphql } from 'gatsby';
 import { Index } from 'elasticlunr';
 import debounce from 'lodash.debounce';
+import useEventListener from '../hooks/use-event-listener';
 import Layout from '../components/Layout';
-import SearchForm from '../components/SearchForm';
-import SearchResults from '../components/SearchResults';
+// import SearchForm from '../components/SearchForm';
+// import SearchResults from '../components/SearchResults';
 
 const Search = ({ data, location }) => {
   const [searchIndex, setSearchIndex] = useState(null);
   const [results, setResults] = useState([]);
+  const [selectedItemIndex, setSelectedItemIndex] = useState(0);
   const searchQuery =
     new URLSearchParams(location.search).get('keywords') || '';
 
@@ -30,10 +32,92 @@ const Search = ({ data, location }) => {
     if (!searchQuery) setResults([]);
   }, [location.search]);
 
+  const keyUpHandler = useCallback(event => {
+    console.log(event.key);
+    if (event.target.id !== 'search-input') {
+      return;
+    }
+
+    switch (event.key) {
+      case 'ArrowUp':
+        setSelectedItemIndex(
+          selectedItemIndex > 0 ? selectedItemIndex - 1 : results.length - 1
+        );
+        break;
+
+      case 'ArrowDown':
+        setSelectedItemIndex(
+          selectedItemIndex < results.length - 1 ? selectedItemIndex + 1 : 0
+        );
+        break;
+
+      default:
+    }
+  });
+
+  // Add event listener using our hook
+  useEventListener('keyup', keyUpHandler);
+
   return (
     <Layout location={location} title="Search">
-      <SearchForm query={searchQuery} />
-      <SearchResults query={searchQuery} results={results} />
+      <label htmlFor="search-input" className="sr-only">
+        Search this site
+      </label>
+      <div
+        role="combobox"
+        aria-expanded={!!results.length}
+        aria-owns="search-results-listbox"
+        aria-haspopup="listbox"
+      >
+        <input
+          type="search"
+          id="search-input"
+          aria-autocomplete="list"
+          aria-controls="search-results-listbox"
+          onChange={e =>
+            navigate(`/search?keywords=${encodeURIComponent(e.target.value)}`)
+          }
+          value={searchQuery}
+        />
+      </div>
+      {!!results.length && searchQuery && (
+        <p
+          className="search-results-count sr-only"
+          id="search-results-count"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          Found {results.length} results for "{searchQuery}"
+        </p>
+      )}
+
+      {!!results.length && (
+        <ol
+          id="search-results-listbox"
+          className="site-search__results-list"
+          role="listbox"
+        >
+          {results.map(({ table, name, url, description, otherNames }, i) => (
+            <li
+              key={name}
+              role="option"
+              aria-selected={i === selectedItemIndex}
+              className="site-search__result"
+            >
+              {table && (
+                <p className="font-sans mb-2 uppercase text-grey-700 text-xs inline-block">
+                  {table === 'Components' ? 'Component' : 'Design System'}
+                </p>
+              )}
+              <h3 className="mt-0">
+                <a href={url}>{name}</a>
+              </h3>
+              {otherNames && <p>Other names: {otherNames}</p>}
+              {description && <p>{description}</p>}
+            </li>
+          ))}
+        </ol>
+      )}
     </Layout>
   );
 };
