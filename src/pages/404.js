f550336e1beefbee7aa0f2e78bf7a5eb51c6f932
Iain Bean <iainmbean@gmail.com>
2019-10-01 18:48:27 +0100

    Update 404 page content

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import Layout from '../components/Layout';
+import Hero from '../components/Hero';
 import SEO from '../components/SEO';
 
 const NotFoundPage = () => (
-  <Layout>
+  <Layout heroComponent={<Hero title="404" />}>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="col-wrap">
+      {/* Main content */}
+      <div className="col col--main pt-4 px-6">
+        <div className="body-text mb-4">
+          <p>It looks like you’ve hit a route that doesn’t exist.</p>
+          <p>
+            You can use the main navigation links above, or try using the search
+            input on the <Link to="/">homepage</Link> to find what you’re
+            looking for.
+          </p>
+        </div>
+      </div>
+    </div>
   </Layout>
 );
 
