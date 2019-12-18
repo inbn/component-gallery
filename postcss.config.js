9c6a279824eda109358fdbe5da9663a67b1380a3
Iain Bean <iainmbean@gmail.com>
2019-12-18 21:16:41 +0000

    Remove all SVG code from src CSS files:
    Use forked version of postcss-svg plugin to inline SVG files as CSS URLs

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 const postcssImport = require('postcss-import');
 const tailwindcss = require('tailwindcss');
 const postcssPresetEnv = require('postcss-preset-env');
+const postcssSVG = require('postcss-svg');
 
 module.exports = {
   plugins: [
@@ -8,6 +9,7 @@ module.exports = {
     tailwindcss('./tailwind.config.js'),
     postcssPresetEnv({
       stage: 0
-    })
+    }),
+    postcssSVG({ dirs: ['./src/svg'] })
   ]
 };
