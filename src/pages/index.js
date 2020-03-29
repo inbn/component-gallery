48d35c8373101a87eefa90b4d58ae00b8b8dc015
Iain Bean <iainmbean@gmail.com>
2020-03-29 16:53:26 +0100

    Add most recently updated Design Systems to homepage, update changelog

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 
 import Component from '../components/Component';
+import DesignSystem from '../components/DesignSystem';
 import Layout from '../components/Layout';
 import ReadMoreLink from '../components/ReadMoreLink';
 import SEO from '../components/SEO';
@@ -51,9 +52,38 @@ const IndexPage = ({ data }) => (
         )}
       </ul>
     </div>
-
     <div className="text-right px-6 py-4 border-l">
       <ReadMoreLink to="/components" text="View all components" />
+    </div>
+    <div className="flex flex-col sm:flex-row-reverse sm:border-t">
+      <h2 className="px-6 py-4 sm:pt-6 text-base uppercase tracking-widest sm:dir-sideways-tb sm:leading-none">
+        Recently added Design Systems
+      </h2>
+      <ul className="grid border-t sm:border-t-0 border-l mt-0 sm:flex-1">
+        {data.recentDesignSystems.edges.map(
+          ({
+            node: {
+              data: { url, name, organisation, image, features, color },
+              id
+            }
+          }) => {
+            return (
+              <DesignSystem
+                key={id}
+                name={name}
+                url={url}
+                organisation={organisation}
+                image={image.localFiles.length > 0 ? image.localFiles[0] : null}
+                features={features}
+                color={color}
+              />
+            );
+          }
+        )}
+      </ul>
+    </div>
+    <div className="text-right px-6 py-4 border-l">
+      <ReadMoreLink to="/design-systems" text="View all Design Systems" />
     </div>
   </Layout>
 );
@@ -93,6 +123,42 @@ export const query = graphql`
             slug: Slug
             otherNames: Other_names
             examplesCount: Examples_count
+          }
+          id
+        }
+      }
+    }
+    recentDesignSystems: allAirtable(
+      filter: {
+        table: { eq: "Design systems" }
+        data: { Publish: { eq: true } }
+      }
+      sort: { fields: [data___Last_reviewed], order: DESC }
+      limit: 8
+    ) {
+      edges {
+        node {
+          data {
+            name: Name
+            organisation: Organisation
+            url: URL
+            image: Image {
+              localFiles {
+                childImageSharp {
+                  fluid(
+                    maxWidth: 608
+                    maxHeight: 456
+                    traceSVG: { background: "#fff", color: "#dae1e7" }
+                  ) {
+                    ...GatsbyImageSharpFluid
+                    ...GatsbyImageSharpFluid_tracedSVG
+                  }
+                }
+              }
+            }
+            features: Features
+            color: Colour_hex
+            Component_examples_count
           }
           id
         }
