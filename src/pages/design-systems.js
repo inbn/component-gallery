abe01ee7fc402895eb479cf6902e2c62c62468bb
Iain Bean <iainmbean@gmail.com>
2020-07-24 21:32:48 +0100

    Fix for loading both base64 and svg design-systems placeholder images

--- src/pages/design-systems.js
+++ src/pages/design-systems.js
@@ -95,7 +95,6 @@ export const query = graphql`
                     maxHeight: 456
                     traceSVG: { background: "#fff", color: "#dae1e7" }
                   ) {
-                    ...GatsbyImageSharpFluid
                     ...GatsbyImageSharpFluid_tracedSVG
                   }
                 }
