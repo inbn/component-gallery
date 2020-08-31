3a78e2ed2d1f598285ef0e9d6811259f03d4db23
Iain Bean <iainmbean@gmail.com>
2020-08-31 09:53:47 +0100

    Add gatsby-plugin-preload-fonts package

--- gatsby-config.js
+++ gatsby-config.js
@@ -213,6 +213,7 @@ module.exports = {
       }
     },
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-preload-fonts`,
     `gatsby-plugin-netlify`,
     `gatsby-plugin-netlify-cache`,
     `gatsby-plugin-preact`,
