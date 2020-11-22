1bf18a459ca749dd90187bf029d3fed6dd5decc0
Iain Bean <iainmbean@gmail.com>
2020-11-22 21:20:52 +0000

    Add dark mode toggle to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,13 @@
 // .storybook/main.js
 
 module.exports = {
+  stories: ['../src/**/*.stories.@(js|mdx)'],
   addons: [
     {
-      name: '@storybook/addon-essentials'
+      name: '@storybook/addon-essentials',
+      options: {
+        backgrounds: false
+      }
     }
   ]
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,34 @@
-import { configure } from '@storybook/react';
 import 'loki/configure-react';
 import { action } from '@storybook/addon-actions';
+import React from 'react';
+import Helmet from 'react-helmet';
 
 import '../src/css/style.css';
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.js$/), module);
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      // Full list of icons in node_modules/@storybook/components/dist/icon/icons.js
+      // icon: 'circlehollow',
+      // array of plain string values or MenuItem shape (see below)
+      items: ['light', 'dark']
+    }
+  }
+};
+
+const withThemeProvider = (Story, context) => {
+  return (
+    <>
+      <Helmet htmlAttributes={{ class: context.globals.theme }} />
+      <Story {...context} />
+    </>
+  );
+};
+
+export const decorators = [withThemeProvider];
 
 // Gatsby's Link overrides:
 // Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
