52770d0b6b4615c7ba5538eda6ddaec19b32b137
Iain Bean <iainmbean@gmail.com>
2021-07-12 18:20:54 +0100

    Fix jump to top when selecting filter

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,6 +12,21 @@ import App from './src/components/App';
 
 require('./src/css/prism-theme.css');
 
-export const wrapRootElement = ({ element }) => {
-  return <App>{element}</App>;
+export const wrapRootElement = ({ element }) => <App>{element}</App>;
+
+// TODO remove this if gatsby-plugin-use-query-params is updated
+// See: https://github.com/alexluong/gatsby-packages/issues/33
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  // hack to override `shouldUpdateScroll` behavior to bypass useQueryParams in product-checkout
+  if (
+    location.search.includes('features=') ||
+    location.search.includes('tech=')
+  ) {
+    return false;
+  }
+  const currentPosition = getSavedScrollPosition(location);
+  return currentPosition || [0, 0];
 };
